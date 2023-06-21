523c6c4cb63e68ebb18fa5f152711f23083e8d0c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-21 16:51:43 +0200

    Functions-Tasks-3-keyword

--- Functions-Tasks.py
+++ Functions-Tasks.py
@@ -18,10 +18,18 @@ mysum(5,10)
 mysum(5,50)
 print('----------')
 '''
-# -2
+# -2 ,
+'''
 def mysum1(x,y):
     return x+y
     
 b = mysum1(5,10)
 print(b)
+'''
+# -3
 
+def mysum1(x,y):
+    return x+y
+    
+b = mysum1(y=4,x=6)
+print(b)
