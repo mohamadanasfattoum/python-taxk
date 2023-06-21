d6e0b4061f5d9ceb52909130c226d788223d3797
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-21 11:56:43 +0200

    task

---
+++ taxk-loop.py
@@ -0,0 +1,65 @@
+# while looop
+...
+x = 0
+while x < 10 :
+    print(x)
+    x +=1
+...
+
+...
+x = 1
+while x <= 5:
+    y = 1
+    while y <=10 :
+        print(f'{x}X{y} = {x*y}')
+        y +=1
+
+    print('---------------')
+    x += 1
+...
+
+# for loop
+
+
+for x in [1,2,3,4,5] :
+    print(x)
+print('---------------')
+
+...
+
+list(range(10,20))
+print('---------------')
+...
+
+list(range(10,100,3))
+print('---------------')
+...
+
+for x in range(1):
+    for y in range(101):
+        print(x+y)
+print('---------------')
+...
+list(range(1,101))
+print('---------------')
+...
+x = 0
+while x<=100:
+    print(x)
+    x +=1
+print('---------------')
+...
+for x in range(1,101):
+    print(x)
+print('---------------')
+...
+for x in range(1,101,3):
+    print(x)
+print('---------------')
+...
+for x in range(1,6):
+    for y in range(1,11):
+        print(f'{x} X {y} = {x*y}')
+    print('---------------')    
+print('---------------')
+
