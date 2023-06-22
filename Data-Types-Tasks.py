12aa537878f4d2dc3573b7d4b90a71183464736d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-22 19:16:49 +0200

    Data-Types-Tasks-2

--- Data-Types-Tasks.py
+++ Data-Types-Tasks.py
@@ -1,6 +1,11 @@
 # -1 With is the differences between ‘ ’ “ ” ‘’’ ‘’’
-
+'''
 print('There is no difference except in certain cases')
 print("Or if they are used inside each other")
 print('Three signals ``` used to write a comment')
 
+'''
+
+# -2
+
+print('mystero')
