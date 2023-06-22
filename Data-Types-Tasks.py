b2c6ecff8ee85842ef58880d9c9a458162059515
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-22 19:08:39 +0200

    Data-Types-Tasks-1

---
+++ Data-Types-Tasks.py
@@ -0,0 +1,6 @@
+# -1 With is the differences between ‘ ’ “ ” ‘’’ ‘’’
+
+print('There is no difference except in certain cases')
+print("Or if they are used inside each other")
+print('Three signals ``` used to write a comment')
+
