60c555c13f39d8620100f96a1162e42b4a346563
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-03 00:03:43 +0200

    add-dictionaries-11-12

--- Data-Types-Tasks.py
+++ Data-Types-Tasks.py
@@ -53,8 +53,15 @@ print(l)
 '''
 
 # -9 -10
+'''
 t= (1,2,3,4,5)
 
 print(t)
 print(type(t))
 print('we can not add 10 to the end of the tuple')
+'''
+# -11, -12
+'''
+d={'anas':70 , 'ahmad':30}
+print(d['anas'])
+'''
