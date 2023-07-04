8aee311972a63d4c8ef051dec1e020c7cc2d0f71
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-04 19:53:57 +0200

    2-task-names-list-but-not-fineshed

---
+++ 2-task-names.py
@@ -0,0 +1,13 @@
+class Name:
+
+    def name_length():
+        names_length = (anas, ali, ahmad)
+
+        for x in name_length (anas, ali, ahmad):
+
+                print(x)
+
+
+n1= Name()
+n1.name_length()
+            
