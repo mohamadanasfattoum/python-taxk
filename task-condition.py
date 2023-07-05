32d83633ab35ecd97061403459a92d735ff97e7f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-05 18:18:10 +0200

    2-task-condition-end

--- task-condition.py
+++ task-condition.py
@@ -1,6 +1,6 @@
 # condition
 # -1
-
+'''
 x=10
 y=15
 if x > y :
@@ -9,3 +9,13 @@ if x > y :
 
 else:
         print('no')
+'''
+'''
+x = 4
+y = 6
+z = 7
+if x and y and z in [2,4,6,8,10]:
+    print('yes')
+else:
+    print('no')
+'''
