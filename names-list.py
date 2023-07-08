0020f0ca515f5ceb165c8744680487fb85f87ca6
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-09 00:26:39 +0200

    add-Functions-name

---
+++ names-list.py
@@ -0,0 +1,21 @@
+'''
+names=('anas','ahmad','ali')
+print(len('anas'))
+
+'''
+
+'''
+names=['anas','ahmad','ali']
+length=[]
+for x in names:
+    length.append(len(x))
+print(length)    
+
+'''
+
+
+def bool_to_string(flag): str('flag') ;print(str) 
+   
+
+
+bool_to_string(flag)
