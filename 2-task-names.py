9fb2af3c397c97370181022e3e20eccd0d5eb546
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-11 16:37:51 +0200

    add-game-name

--- 2-task-names.py
+++ 2-task-names.py
@@ -1,13 +1,25 @@
-class Name:
+'''
+names = ['anas','ali','ahmad','omar']
 
-    def name_length():
-        names_length = (anas, ali, ahmad)
+length = []
+for x in names:
+    length.append(len(x))
 
-        for x in name_length (anas, ali, ahmad):
-
-                print(x)
+print(length)
 
 
-n1= Name()
-n1.name_length()
+result = [len(x) for x in names]
+print (result)
+'''
+
+
+# Game input names and length with list comprehensions
+ #my game
+'''
+names = input('Enter names : ')
+names_list = names.split(',')
+length = int(input('Enter Length : '))
+result2 = [x for x in names_list if len(x) >= length]
+print (result2)
+'''
             
